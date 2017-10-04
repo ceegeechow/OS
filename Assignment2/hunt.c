0f98869746e0435f8177df5b84bd80463c424ebd
ceegeechow <camille@chowww.com>
2017-10-04 17:35:17 -0400

    changed var names

--- Assignment2/hunt.c
+++ Assignment2/hunt.c
@@ -11,8 +11,8 @@
 #include <unistd.h>
 
 //Global Variables - stats for target file
-ino_t inodenum;
-off_t filesize;
+ino_t target_ino;
+off_t target_size;
 
 //Function to compare contents of files
 //returns 1 if identical
@@ -39,66 +39,71 @@ void searchFiles(char *directory) {
         char path[1024];
         sprintf(path, "%s/%s", directory, entry->d_name);
         
-        //run stat on entry
-        struct stat st;
-        if (stat(path,&st) < 0) {
-            fprintf(stderr, "Warning: Could not run stat on file %s: %s\n", directory, strerror(errno));
-        }
-        //get stats
-        ino_t ino = st.st_ino; //inode number
-        off_t size = st.st_size; //size in bytes
-        
-        //check if entry is another directory
-        if (entry->d_type == DT_DIR) {                      //should i check type of entry using mode???
+        //if entry is another directory, recursively search
+        if (entry->d_type == DT_DIR) {
             if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
                 continue;
             searchFiles(path);
         }
-        //check for symlink
-        else if (entry->d_type == DT_LNK) {                     //how to test symlink? alias doesn't work
-            //resolves to target
-            if (ino == inodenum) {
-                printf("%s\tSYMLINK RESOLVES TO TARGET\n",path);
-            }
-            //resolves to duplicate
-            else if (size == filesize && compareFiles(path) == 1) {
-                //find contents of link
-                char link[1024];
-                if (readlink(path,link,sizeof(link)) < 0) {
-                    fprintf(stderr, "Warning: Could check contents of symlink %s: %s\n", path, strerror(errno));
-                }
-                else {
-                   printf("%s\tSYMLINK (%s) RESOLVES TO DUPLICATE\n",path,link);
-                }
-                
-            }
-        }
-        //if regular file, check for hardlink or duplicate
-        else if (entry->d_type == DT_REG) {
-            //get permissions
-            mode_t mode = st.st_mode;
-            int o_permissions = (mode & S_IROTH); //'other' read permissions          //doesn't work? check with chmod?
-            char* perm_string;
-            if (o_permissions == 1) {
-                perm_string = "OK READ by OTHER";
-            }
-            else {
-                perm_string = "NOT READABLE by OTHER";
-            }
-
-            //check for hardlink
-            if (ino == inodenum) {
-                printf("%s\tHARD LINK TO TARGET\t%s\n",path,perm_string);
+        //not directory
+        else {
+            
+            //run stat on entry
+            struct stat st;
+            if (stat(path,&st) < 0) {
+                fprintf(stderr, "Warning: Could not run stat on file %s: %s\n", directory, strerror(errno));
             }
             
-            //check for duplicate
-            else if (size == filesize && compareFiles(path) == 1) {
-                nlink_t links = st.st_nlink;
-                printf("%s\tDUPLICATE OF TARGET\tnlink=%d\t%s\n",path,links,perm_string);
+            //get stats
+            ino_t ino = st.st_ino; //inode number
+            off_t size = st.st_size; //size in bytes
+            
+            //symlink
+            if (entry->d_type == DT_LNK) {                     //how to test symlink? alias doesn't work
+                //resolves to target
+                if (ino == target_ino) {
+                    printf("%s\tSYMLINK RESOLVES TO TARGET\n",path);
+                }
+                //resolves to duplicate
+                else if (size == target_size && compareFiles(path) == 1) {
+                    //find contents of link
+                    char link[1024];
+                    if (readlink(path,link,sizeof(link)) < 0) {
+                        fprintf(stderr, "Warning: Could check contents of symlink %s: %s\n", path, strerror(errno));
+                    }
+                    else {
+                        printf("%s\tSYMLINK (%s) RESOLVES TO DUPLICATE\n",path,link);
+                    }
+                }
             }
-        }
-        else {
-            fprintf(stderr, "Directory entry %s not a directory, regular file, or symlink, skipping\n", directory);
+            //regular file
+            else if (entry->d_type == DT_REG) {
+                //get permissions
+                mode_t mode = st.st_mode;
+                int o_permissions = (mode & S_IROTH); //'other' read permissions          //doesn't work? check with chmod?
+                char* perm_string;
+                if (o_permissions == 1) {
+                    perm_string = "OK READ by OTHER";
+                }
+                else {
+                    perm_string = "NOT READABLE by OTHER";
+                }
+                
+                //check for hardlink
+                if (ino == target_ino) {
+                    printf("%s\tHARD LINK TO TARGET\t%s\n",path,perm_string);
+                }
+                
+                //check for duplicate
+                else if (size == target_size && compareFiles(path) == 1) {
+                    nlink_t links = st.st_nlink;
+                    printf("%s\tDUPLICATE OF TARGET\tnlink=%d\t%s\n",path,links,perm_string);
+                }
+            }
+            //other file type
+            else {
+                fprintf(stderr, "Directory entry %s not a directory, regular file, or symlink, skipping\n", path);
+            }
         }
     }
     closedir(dir);
@@ -108,7 +113,7 @@ void searchFiles(char *directory) {
 int main(int argc, char**argv) {
     
     char* targetname = argv[1];
-    char* dirname = argv[2];
+    char* starting_directory = argv[2];
     DIR* dir;
     struct stat st;
     
@@ -116,15 +121,15 @@ int main(int argc, char**argv) {
         fprintf(stderr, "FATAL ERROR: Could not run stat on target file '%s': %s\n", targetname, strerror(errno));
         return -1;
     }
-    else if (!(dir = opendir(dirname))) {
-        fprintf(stderr, "FATAL ERROR: Could not open directory '%s': %s\n", dirname, strerror(errno));
+    else if (!(dir = opendir(starting_directory))) {
+        fprintf(stderr, "FATAL ERROR: Could not open directory '%s': %s\n", starting_directory, strerror(errno));
         return -1;
     }
-    inodenum = st.st_ino;
-    filesize = st.st_size;
-    nlink_t links = st.st_nlink;
-    printf("Target inode number:%llu\nTarget file size:%lld\nTarget nlink:%hu\n",inodenum,filesize,links);
-    searchFiles(dirname);
+    target_ino = st.st_ino;
+    target_size = st.st_size;
+    //nlink_t links = st.st_nlink;
+    //printf("Target inode number:%llu\nTarget file size:%lld\nTarget nlink:%hu\n",target_ino,target_size,links);
+    searchFiles(starting_directory);
     
     return 0;
 }
