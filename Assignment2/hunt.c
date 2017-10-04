e6b127d7dd8aaa224ee6a37d1bff3a16c1a661ad
ceegeechow <camille@chowww.com>
2017-10-04 02:59:06 -0400

    doesn't work, no symlinks

--- Assignment2/hunt.c
+++ Assignment2/hunt.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+//#include <unistd.h>
 
 //Global Variables - stats for target file
 ino_t inodenum;
@@ -36,7 +37,7 @@ void searchFiles(char *name)            //should name be const?
                 continue;
             char path[1024];
             sprintf(path, "%s/%s", name, entry->d_name);
-            //printf("[%s]\n", entry->d_name);
+            printf("[%s]\n", entry->d_name);
             searchFiles(path);
             continue;                                           //does this work???
         }
@@ -52,19 +53,29 @@ void searchFiles(char *name)            //should name be const?
             if (stat(name,&st) < 0) {
                 fprintf(stderr, "Warning: Could not run stat on file %s: %s\n", name, strerror(errno));
             }
-            ino_t ino = st.st_ino;
-            mode_t mode = st.st_mode;
-            //find permissions
-            off_t size = st.st_size;
+            ino_t ino = st.st_ino; //inode number
+            mode_t mode = st.st_mode; //mode
+            int o_permissions = (mode & S_IROTH); //other read permissions
+            char* perm_string;
+            if (o_permissions == 1) {
+                perm_string = "OK READ by OTHER";
+            }
+            else {
+                perm_string = "NOT READABLE by OTHER";
+            }
+            off_t size = st.st_size; //size in bytes
             
             //check for hardlink
             if (ino == inodenum) {
                 //hardlink: print path, "hardlink", permissions
+                printf("%s\tHARD LINK TO TARGET\t%s\n",name,perm_string);
             }
             
             //check for duplicate
             else if ((size == filesize) && (compareFiles(name) == 1)) {
                 //duplicate: print path, "duplicate of target", nlink, permissions
+                nlink_t links = st.st_nlink;
+                printf("%s\tDUPLICATE OF TARGET\tnlink=%d\t%s\n",name,links,perm_string);
             }
         }
         else {
