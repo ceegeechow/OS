d5b0913316b654ea201a529681ef2590e570a6ce
ceegeechow <camille@chowww.com>
2017-10-23 22:24:39 -0400

    real time doesn't work

--- Assignment3/shell.c
+++ Assignment3/shell.c
@@ -43,8 +43,10 @@ void procLine(char* line)
     char* token = strtok(line,d);
     int arg_num = 0;
     struct rusage ru;
-    //time_t* start;
-    //time_t* end;
+    time_t start;
+    time_t end;
+    time_t s;
+    time_t e;
     
     while (token != NULL)
     {
@@ -62,21 +64,19 @@ void procLine(char* line)
     }
     else if (command[0][0] != '#')
     {
-        pid_t pid = fork();
-        
-        switch (pid)
+        int pid;
+        switch (pid = fork())
         {
             case -1:
                 fprintf(stderr,"Error forking: %s\n", strerror(errno)); //more info??
                 
             case 0:
-                //time(start);
+                s = time(&start);
                 if (execvp(command[0],command) < 0)
                 {
                     fprintf(stderr,"Error executing command '%s': %s\n", command[0], strerror(errno));
                 }
-                //time(end);
-                //fprintf(stderr,"time recorded\n");
+                //do i have to do anything to clean environment/exit child???
                 break;
                 
             default:
@@ -84,6 +84,8 @@ void procLine(char* line)
                 {
                     fprintf(stderr,"Error waiting for child (executing %s): %s\n", command[0], strerror(errno));
                 }
+                e = time(&end);
+                fprintf(stderr, "time: %f seconds\n", difftime(e, s));
                 //fprintf(stderr, "Process cosumed %ld real seconds, %ld.%.6d user, %ld.%.6d system\n", (*end - *start), ru.ru_utime.tv_sec, ru.ru_utime.tv_usec, ru.ru_stime.tv_sec, ru.ru_stime.tv_usec);
                 break;
         }
