fad8479bf2fc1cc054dbfc7b65e87ee4ff602fe6
ceegeechow <camille@chowww.com>
2017-11-15 12:35:10 -0500

    working

--- Assignment4/wordgen.c
+++ Assignment4/wordgen.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-//print same length words
-//prints same words every time (need to use seed?)
 //do i need to print how many words were generated?
 
 int main(int argc, char** argv)
 {
-    int nc = 10;
+    int nc = 5; //max word length
     int m, wordcount;
     if (argc > 1)
         wordcount = atoi(argv[1]);      //try catch?
-
+    srand((unsigned) time(NULL));
+    char* word;
     while (argc == 1 || wordcount > 0)
     {
         m = rand()%(nc-2)+3;
-        char* word = malloc(m);         //necessary??
+        word = malloc(m);         //necessary??
         for (int i = 0; i < m; i++)
             word[i] = rand()%26 + 65;
         fprintf(stdout, "%s\n", word);
+        memset(word,0,m);               //better way?
         free(word);
         wordcount--;
     }
