30c8884df1a89df497ecc8f99618feecb45b6939
ceegeechow <camille@chowww.com>
2017-11-21 12:10:09 -0500

    finished main

--- Assignment4/launcher.c
+++ Assignment4/launcher.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/resource.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
 
 void redir(int fd_pipe, int fd_std)
 {
     if (dup2(fd_pipe, fd_std) < 0) {
-        fprintf(stderr, "Can’t dup2 fd %d to fd %d: %s\n", fd, fd_pipe, strerror(errno));
+        fprintf(stderr, "Can’t dup2 fd %d to fd %d: %s\n", fd_std, fd_pipe, strerror(errno));
         _exit(-1);
     }
     if (close(fd_pipe) != 0) {
-        fprintf(stderr, "Error closing file descriptor %d: %s\n", fd, strerror(errno));
+        fprintf(stderr, "Error closing file descriptor %d: %s\n", fd_pipe, strerror(errno));
         _exit(-1);
     }
 }
@@ -26,19 +29,19 @@ int main(int argc, char** argv)
     if (pipe(pipe1fd) == -1)
     {
         fprintf(stderr, "Error creating first pipe: %s\n", strerror(errno));
-        return -1;
+        _exit(-1);
     }
     if (pipe(pipe2fd) == -1)
     {
         fprintf(stderr, "Error creating second pipe: %s\n", strerror(errno));
-        return -1;
+        _exit(-1);
     }
     //forks
     int pid = fork();
     if (pid == -1)
     {
         fprintf(stderr, "Error forking: %s\n", strerror(errno));
-        return -1;
+        _exit(-1);
     }
     else if (pid == 0) //child - wordgen
     {
@@ -55,12 +58,45 @@ int main(int argc, char** argv)
         if (pid2 == -1)
         {
             fprintf(stderr, "Error forking: %s\n", strerror(errno));
-            return -1;
+            _exit(-1);
         }
         else if (pid2 == 0) //child - wordsearch
         {
-            
+            redir(pipe1fd[1], 0); //redirect stdin to output of first pipe
+            redir(pipe2fd[0], 1); //redirect stdout to input of second pipe
+            if (execlp("wordsearch","wordsearch","dict.txt") < 0)
+            {
+                fprintf(stderr,"Error executing wordsearch: %s\n", strerror(errno));
+                _exit(-1);
+            }
         }
+        else //parent - launcher
+        {
+            int pid3 = fork();
+            if (pid3 == -1)
+            {
+                fprintf(stderr, "Error forking: %s\n", strerror(errno));
+                _exit(-1);
+            }
+            else if (pid3 == 0) //child - pager
+            {
+                redir(pipe2fd[1], 0); //redirect stdin to output of second pipe
+                if (execlp("pager","pager") < 0)
+                {
+                    fprintf(stderr,"Error executing pager: %s\n", strerror(errno));
+                    _exit(-1);
+                }
+            }
+        }
+    }
+    int status = -1;
+    int p;
+    for (int i = 0; i < 3; i++)
+    {
+    if ((p = wait(&status)) < 0)
+        fprintf(stderr,"Error waiting for child: %s\n", strerror(errno));
+    else
+        fprintf(stderr, "Child %d exited with status %d", p, status);
     }
     return 0;
 }
