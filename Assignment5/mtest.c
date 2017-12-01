a2e58afe629f279fb9ea46861c961c2fbb40e79b
ceegeechow <camille@chowww.com>
2017-12-01 12:23:44 -0500

    test1 draft1

--- Assignment5/mtest.c
+++ Assignment5/mtest.c
@@ -3,10 +3,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+//#include <unistd.h>
 
 void test1()
 {
+    int fd;
+    struct stat sb;
+    char* map;
+    char* name = "test1.txt";
+    //set signal handlers for signals 1 through 31
+    for (int i = 1; i < 32; i++)
+        signal(i,hand1);
+    //open test file
+    if ((fd = open(name, O_RDONLY)) < 0)    //read only????
+    {
+        fprintf(stderr, "Could not open test file for reading: %s\n", strerror(errno));
+        exit(255);
+    }
+    //get size
+    if (fstat(fd, &sb) < 0)
+    {
+        fprintf(stderr, "Error stat-ing test file: %s\n", strerror(errno));
+        exit(255);
+    }
+    //call mmap
+    if ((map = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0)) < 0)
+        //are these the right arguments??? shared/private, len, offset??
+    {
+        fprintf(stderr, "Error mmap-ing test file: %s\n", strerror(errno));
+        exit(255);
+    }
+    //attempt to write
+    //??????????????????????????????
+    //unmap and close
+    if (munmap(map, sb.st_size) < 0)
+    {
+        fprintf(stderr, "Error unmapping mmap: %s\n", strerror(errno));
+        exit(255);
+    }
+    if (close(fd) < 0)
+    {
+        fprintf(stderr, "Error closing test file: %s\n", strerror(errno));
+        exit(255);
+    }
     exit(0);
+}
+
+void hand1(int sig)
+{
+    fprintf(stderr, "Signal %s received\n", strsignal(sig));
+    exit(sig);
 }
 
 void test2()
@@ -36,7 +88,7 @@ void test6()
 
 int main(int argc, char**argv)
 {
-    if (argc == 1)
+    if (argv[1] == NULL)
     {
         fprintf(stderr, "Please specify test number\n");
         return 0;
@@ -59,4 +111,5 @@ int main(int argc, char**argv)
             fprintf(stderr, "Test number must be between 1 and 6\n");
             return 0;
     }
+    return 0;
 }
