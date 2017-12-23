74614978cc4b6f4ca5be77e4a9c51db187c7857b
ceegeechow <camille@chowww.com>
2017-12-22 22:11:34 -0800

    problem 2 works

--- Assignment6/spintest.c
+++ Assignment6/spintest.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 
 int main(int argc, char**argv) {
     if (argc < 3)
@@ -16,28 +15,27 @@ int main(int argc, char**argv) {
     }
     int nchild = atoi(argv[1]);
     int niter = atoi(argv[2]);
-    struct spinlock sl;
     int* map;
-    sp_init(&sl);
     if ((map = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_ANON|MAP_SHARED, -1, 0)) < 0)
     {
         fprintf(stderr, "Error mmap-ing: %s\n", strerror(errno));
         exit(255);
     }
     map[0] = 0;
+    struct spinlock *slp = (struct spinlock *) &map[1];
+    sp_init(slp);
     pid_t pid;
     for (int i = 0; i < nchild; i++) {
         switch (pid = fork()) {
             case -1:
                 fprintf(stderr,"Error forking: %s\n", strerror(errno));
-                break;
-                
+                break;    
             case 0:
-                spin_lock(&sl);
+                spin_lock(slp);
                 for (int j = 0; j < niter; j++) {
                     map[0]++;
                 }
-                spin_unlock(&sl);
+                spin_unlock(slp);
                 _exit(0);
                 break;
         }
